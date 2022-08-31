775fe13a1bd0787d46c448f763765647c3a7d49f
mgon123 <106345774+mgon123@users.noreply.github.com>
2022-08-30 20:30:05 -0400

    Hello World working

--- client/App.jsx
+++ client/App.jsx
@@ -4,8 +4,10 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-        <h1> Hello, World!</h1>
+        <h1> Hello, Losers!</h1>
       </div>
     );
   }
 }
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env', '@babel/preset-react'] }
+      },
+    ]
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    // publicPath: '/dist/',
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './client/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    // contentBase: path.join(__dirname, 'client/'),
+    // port: 3000,
+    // publicPath: 'http://localhost:3000/',
+    // hotOnly: true
+  },
+};
